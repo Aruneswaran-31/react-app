74cdada1df16c95ae96c301a5a402e93a559c156
Sharmila642 <sharmilapandi642@gmail.com>
2024-12-20 11:26:18 +0530

    Initialized

---
+++ src/components/pages/NewsItem.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function NewsItem({article}) {
+  return (
+    <div style={{
+         border:"1px solid #ddd",
+         borderRadius:"10px",
+         padding:"15px",
+         margin:"10px",
+         maxWidth:"600px",
+         marginLeft:"auto",
+         marginRight:"auto",
+    }}>
+        <a href={article.url} target="_blank" rel="noreferrer">
+          <h3>{article.title}</h3>  
+          <p><strong>Source:</strong>{article.source.name}</p> 
+          <img src={article.urlToImage} alt={article.title} style={{width:"100%",borderRadius:"10px"}}></img>
+        </a>
+    </div>
+  )
+}
+
+export default NewsItem
---
+++ src/components/pages/Todo.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+
+function Todo() {
+  const[task,setTask]=useState("");
+  const[tasks,setTasks]=useState([]);
+  const[completed,setCompleted]=useState(true);
+  const handleSubmit=(e) => {
+    e.preventDefault();
+    if(task.trim()){
+        setTasks([...tasks,{id:Date.now().toString(),name:task, completed:false}]);
+        setTask("");
+    }
+  };
+  function toggleCompleted(id){
+    setTasks(
+      tasks.map((t) =>t.id === id?{...t,completed: !t.completed}: t) 
+    )
+  }
+  const filterTask = tasks.filter((t) =>completed || !t.completed)
+    return (
+    <div>
+        <h1>To-Do-Application</h1>
+        <form onSubmit={handleSubmit}>
+            <input type='text'placeholder="Today's Task?" 
+            value={task} onChange={(e) => setTask(e.target.value)}/><br/>
+            <button type='submit'>Add</button><br/>
+        </form>
+        <button onClick={() => setCompleted(!completed)}>{completed ?"Hide":"Show"}</button>
+        <ul>
+          {
+            filterTask.map((t) => 
+              <li key={t.id}><span style={{textDecoration: t.completed?'line-through':'none',color:t.completed?'red':'green',
+              }}>{t.name}</span>
+              <button onClick={() => toggleCompleted(t.id)}>
+                {t.completed ? 'Undo': 'Completed'}</button>
+              </li>
+
+            )
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default Todo
